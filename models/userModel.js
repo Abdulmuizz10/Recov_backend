782f54a06ccdb177a330f301cac1b7658c5e98f7
Your Name <harrismuizz10@gmail.com>
2025-08-15 00:15:59 +0100

    first commit

---
+++ models/userModel.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true, // Prevent duplicate emails for registered users
+      required: true,
+      // match: [/.+@.+\..+/, "Please enter a valid email address"], // Basic email validation
+    },
+    password: {
+      type: String,
+      required: function () {
+        return this.authMethod === "password"; // Required only for password-based users
+      },
+      select: false, // Exclude password from queries by default for security
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+
+    authMethod: {
+      type: String,
+      enum: ["password", "google"], // Define possible authentication methods
+      default: "password",
+    },
+    // posts: [
+    //   {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: "PostModel", // Links user to their post records
+    //     required: false,
+    //   },
+    // ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("UserModel", userSchema);
---
+++ server.js
@@ -0,0 +1,46 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+//routes
+import authRoutes from "./routes/authRoutes.js";
+import usersRoutes from "./routes/usersRoutes.js";
+
+dotenv.config();
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+};
+
+// express initialization
+const app = express();
+
+//middlewares
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+//routes declarations
+app.use("/auth", authRoutes);
+app.use("/users", usersRoutes);
+
+// app.use("/create-payment", paymentRoutes);
+
+//database initialization
+const PORT = process.env.PORT || 5000;
+const DATABASE_URL = process.env.MONGO_URL;
+// const DATABASE_URL = process.env.LOCAL_URL;
+mongoose
+  .connect(DATABASE_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server connected to port: ${PORT}`))
+  )
+  .catch((error) =>
+    console.log(`${error}Server is not connected to port: ${PORT}`)
+  );
